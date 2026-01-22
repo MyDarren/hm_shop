a4b53f6c10ce91a6ac466bfc27c0df0f00c6c806
Darren <1540313817@qq.com>
2026-01-22 16:06:58 +0800

    主页tab栏实现

--- lib/pages/main/index.dart
+++ lib/pages/main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/Mine/index.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,15 +12,74 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  // 定义数据 根据数据进行渲染4个导航
+  // 一般应用程序的导航是固定的
+  final List<Map<String, String>> _tabList = [
+    {
+      "text": "首页",
+      "icon": "lib/assets/ic_public_home_normal.png",
+      "active_icon": "lib/assets/ic_public_home_active.png"
+    },
+    {
+      "text": "分类",
+      "icon": "lib/assets/ic_public_pro_normal.png",
+      "active_icon": "lib/assets/ic_public_pro_active.png"
+    },
+    {
+      "text": "购物车",
+      "icon": "lib/assets/ic_public_cart_normal.png",
+      "active_icon": "lib/assets/ic_public_cart_active.png"
+    },
+    {
+      "text": "我的",
+      "icon": "lib/assets/ic_public_my_normal.png",
+      "active_icon": "lib/assets/ic_public_my_active.png"
+    },
+  ];
+
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(_tabList.length, (index) {
+      return BottomNavigationBarItem(
+          icon: Image.asset(
+            _tabList[index]["icon"]!,
+            width: 30,
+            height: 30,
+          ),
+          activeIcon: Image.asset(
+            _tabList[index]["active_icon"]!,
+            width: 30,
+            height: 30,
+          ),
+          label: _tabList[index]["text"]!);
+    });
+  }
+
+  List<Widget> _getChildren() {
+    return [HomeView(), CategoryView(), CartView(), MineView()];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("主页"),
-      ),
-      body: Center(
-        child: Text("主页"),
-      ),
+      //appBar: AppBar(title: Text("主页"),),
+      //body: Center(child: Text("主页")),
+      // SafeArea避开安全区组件
+      body: SafeArea(
+          child: IndexedStack(
+        index: _currentIndex,
+        children: _getChildren(),
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+          showUnselectedLabels: true,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black,
+          onTap: (index) {
+            _currentIndex = index;
+            setState(() {});
+          },
+          currentIndex: _currentIndex,
+          items: _getTabBarWidget()),
     );
   }
 }
--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hm_shop/pages/login/index.dart';
-import 'package:hm_shop/pages/main/index.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
 
 // 返回APP根级组件
 Widget getRootWidget() {
